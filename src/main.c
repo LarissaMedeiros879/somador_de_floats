f12ce78ebc97baeb5950340b8963ccab33529d91
Larissa Medeiros <l178014@DAC.UNICAMP.BR>
2019-03-08 12:34:10 -0300

    work in progress

--- src/main.c
+++ src/main.c
@@ -1,17 +1,62 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
+/* Somador
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+void limpabuffer(char buffer[MAX]) {
+	int i = 0;
+	for (i = 0; i < MAX; i++) {
+		buffer[i] = '\0';
+	}
+}
+
+int efloat (char numero[MAX]) {
+	return 1;
+}
 
 int main() {
-
-  int x, y;
-
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
-  return 0;
+	char c;
+	int i = 0;
+	char buffer[MAX];
+	limpabuffer(buffer);
+	
+	do {
+		c = getchar();
+		buffer[i] = c;
+		i++;
+	}
+	while (c != '\n');
+	
+	//printf("%s\n", buffer);
+	
+	char numero[MAX];
+	limpabuffer(numero);
+	int j = 0;
+	i = 0;
+	float soma = 0;
+	
+	printf("blabla");
+	
+	while (buffer[i] != '\n') {
+		do {
+			numero[j] = buffer[i];
+			i++;
+			j++;	
+		}
+		while (buffer[i] != ' ' || buffer[i] != '\n');
+		//if (efloat(numero)) {
+			//soma = soma + atof(numero);
+		//} 
+		printf("%s", numero);
+		limpabuffer(numero);
+		j = 0;
+	}
+	
+	printf("Resultado= %f\n", soma);
+	
+	
+	return 0;
 }
